ab4aabc29dfa5bc0cc5eaa338fcb33ccfef18cd3
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-05-11 09:30:02 +0700

    Reformat Ngat

--- Ngat/Ngat.c
+++ Ngat/Ngat.c
@@ -1,13 +1,13 @@
-#include<p18f4520.h>
-#include<delays.h>
+#include <p18f4520.h>
+#include <delays.h>
 
+// Trạng thái của LED 5
 char led = 0;
-//char led5 = 0;
 
-#pragma config OSC=HS
-#pragma config WDT=OFF
-#pragma config MCLRE=ON
-#pragma config PBADEN=OFF
+#pragma config OSC = HS
+#pragma config WDT = OFF
+#pragma config MCLRE = ON
+#pragma config PBADEN = OFF
 
 void ngat_ngoai(void);
 
@@ -34,43 +34,40 @@ void ngat_ngoai(void)
 	}
 }
 
-
 void main()
 {
 	ADCON1 = 0x0F;
 	TRISB = 0b00000101;
-	
-	INTCONbits.GIE = 1;
-	INTCONbits.PEIE = 0;
-	INTCONbits.INT0IE = 1;
-	INTCON2bits.INTEDG0 = 0;
-	INTCONbits.INT0IF = 0;
 
-	INTCONbits.GIE = 1;
-	INTCONbits.PEIE = 0;
-	INTCON3bits.INT2IE = 1;
-	INTCON2bits.INTEDG0 = 0;
-	INTCON3bits.INT2IF = 0;
+	INTCONbits.GIE = 1; // Cho phep ngat toan cuc
+
+	INTCONbits.INT0IE = 1;	 // Cho phep ngat ngoai INT0
+	INTCON2bits.INTEDG0 = 0; // Ngat suon am
+	INTCONbits.INT0IF = 0;	 // Xoa co ngat INT0
+
+	INTCON3bits.INT2IE = 1;	 // Cho phep ngat ngoai INT2
+	INTCON2bits.INTEDG0 = 0; // Ngat suon am
+	INTCON3bits.INT2IF = 0;	 // Xoa co ngat INT2
 
 	while (1)
 	{
 		led = led | 0b10000000;
-		led =led & 0b10001000; //Giu lai gia tri cho bit 10001000
+		led = led & 0b10001000; // Giu lai gia tri cho bit 10001000
 		PORTB = led;
 		Delay10KTCYx(50);
 
 		led = led | 0b01000000;
-		led =led & 0b01001000;
+		led = led & 0b01001000;
 		PORTB = led;
 		Delay10KTCYx(50);
 
 		led = led | 0b00100000;
-		led =led & 0b00101000;
+		led = led & 0b00101000;
 		PORTB = led;
 		Delay10KTCYx(50);
 
 		led = led | 0b00010000;
-		led =led & 0b00011000;
+		led = led & 0b00011000;
 		PORTB = led;
 		Delay10KTCYx(50);
 	}
