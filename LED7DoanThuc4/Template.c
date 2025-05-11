ab5a86968daa05054c0dc690d6e2d2f3e3f29f05
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-05-11 16:08:31 +0700

    Thay đổi thuật toán cho led 7 đoạn 4 số thực vị trí dấu chấm ngẫu nhiên

--- LED7DoanThuc4/Template.c
+++ LED7DoanThuc4/Template.c
@@ -11,12 +11,11 @@
 
 unsigned char ma_led[] = {0b11000000, 0b11111001, 0b10100100, 0b10110000, 0b10011001, 0b10010010, 0b10000010, 0b11111000, 0b10000000, 0b10010000};
 unsigned char led[] = {0b00000001, 0b00000010, 0b00000100, 0b00001000};
-unsigned int luyThua10[] = {10, 100, 1000};
+unsigned int luyThua10[] = {1, 10, 100, 1000};
 unsigned char so[] = {0, 0, 0, 0};
 unsigned char i;
-unsigned char dotIndex;
 unsigned int soHienthi;
-float f = 45.67;
+float f = 0.456;
 
 void main(void)
 {
@@ -26,41 +25,38 @@ void main(void)
 	PORTD = 0b00000000;
 	while (1)
 	{
+		soHienthi = (unsigned int)(f);
+		// lấy vị trí dấu chấm thập phân
 		for (i = 0; i <= 3; i++)
 		{
-			if (f / luyThua10[i] < 1)
-			{
-				dotIndex = i;
+			if (soHienthi < 10)
 				break;
-			}
+			else
+				soHienthi = soHienthi / 10;
 		}
-		soHienthi = (unsigned int)(f * luyThua10[2 - dotIndex]);
-		if (soHienthi >= 1000 && soHienthi <= 9999)
-		{
-			so[0] = soHienthi / 1000;
-			so[1] = (soHienthi % 1000) / 100;
-			so[2] = ((soHienthi % 1000) % 100) / 10;
-			so[3] = ((soHienthi % 1000) % 100) % 10;
-		}
-
+		// lấy toàn bộ các chữ số từ f sang soHienthi
+		soHienthi = (unsigned int)(f * luyThua10[3 - i]);
+		// lấy từng chữ số lưu vào các biến
+		so[0] = soHienthi / 1000;
+		so[1] = (soHienthi % 1000) / 100;
+		so[2] = ((soHienthi % 1000) % 100) / 10;
+		so[3] = ((soHienthi % 1000) % 100) % 10;
+		// hiển thị các số trên led
 		PORTD = 0b00000001;
 		PORTC = ma_led[so[0]];
 		Delay10KTCYx(1);
-
 		PORTD = 0b00000010;
 		PORTC = ma_led[so[1]];
 		Delay10KTCYx(1);
-
 		PORTD = 0b00000100;
 		PORTC = ma_led[so[2]];
 		Delay10KTCYx(1);
-
 		PORTD = 0b00001000;
 		PORTC = ma_led[so[3]];
 		Delay10KTCYx(1);
-
-		PORTD = led[dotIndex];
-		PORTC = ma_led[so[dotIndex]] & 0b01111111;
+		// hiển thị dấu chấm thập phân
+		PORTD = led[i];
+		PORTC = ma_led[so[i]] & 0b01111111;
 		Delay10KTCYx(1);
 	}
 }
