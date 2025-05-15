de900788e6d8e7728f500f703033e19223c16129
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-05-15 18:55:28 +0700

    Create PWM

---
+++ BaiTap/6-PWM/PWM/Template.c
@@ -0,0 +1,20 @@
+#include <P18f4520.h>
+#include <adc.h>
+
+#pragma config OSC = HS
+#pragma config MCLRE = ON
+#pragma config WDT = OFF
+#pragma config PBADEN = OFF
+
+void main (void)
+{
+	ADCON1 = 0x0f;
+    
+	TRISC=0;
+	T2CONbits.T2CKPS1=1;  
+	T2CONbits.T2CKPS0=1;
+
+	OpenPWM1(124);
+
+	SetDCPWM1(250);
+}
