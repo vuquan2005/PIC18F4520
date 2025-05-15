53a16a8add1cec11b62c9a0c5f47ea787f531b2e
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-05-15 14:48:56 +0700

    Update ADC

--- BaiTap/5-ADC/ADC/ADC.c
+++ BaiTap/5-ADC/ADC/ADC.c
@@ -6,39 +6,40 @@
 #pragma config WDT = OFF
 #pragma config PBADEN = OFF
 
-void main (void)
+void main(void)
 {
 	unsigned int x;
-	ADCON1=0b00001001;
-	TRISC=0;
-	TRISD=0;
-    TRISEbits.TRISE0=1;
-	TRISAbits.TRISA0=1;
-	TRISBbits.TRISB7=1;
-	
-	OpenADC(ADC_FOSC_4 & ADC_RIGHT_JUST & ADC_2_TAD, 
-			ADC_CH0 & ADC_INT_OFF & ADC_VREFPLUS_VDD & ADC_VREFMINUS_VSS, 
+	ADCON1 = 0x0f;
+	TRISC = 0;
+	TRISD = 0;
+	TRISEbits.TRISE0 = 1;
+	TRISAbits.TRISA0 = 1;
+	TRISBbits.TRISB7 = 1;
+
+	OpenADC(ADC_FOSC_4 & ADC_RIGHT_JUST & ADC_2_TAD,
+			ADC_CH0 & ADC_INT_OFF & ADC_VREFPLUS_VDD & ADC_VREFMINUS_VSS,
 			0b1001);
 	while (1)
 	{
-		if (PORTBbits.RB7==0) //nhan
+		if (PORTBbits.RB7 == 0) // nhan
 		{
-			SetChanADC( ADC_CH0 ); 
+			SetChanADC(ADC_CH0);
 			ConvertADC();
-			while(BusyADC());
-			x=ReadADC();
-			PORTC=x;
-			PORTD=x>>8;
+			while (BusyADC())
+				;
+			x = ReadADC();
+			PORTC = x;
+			PORTD = x >> 8;
 		}
 		else
 		{
-			SetChanADC( ADC_CH5 ); 
+			SetChanADC(ADC_CH5);
 			ConvertADC();
-			while(BusyADC());
-			x=ReadADC();
-			PORTC=x;
-			PORTD=x>>8;
-			
+			while (BusyADC())
+				;
+			x = ReadADC();
+			PORTC = x;
+			PORTD = x >> 8;
 		}
 	}
 }
