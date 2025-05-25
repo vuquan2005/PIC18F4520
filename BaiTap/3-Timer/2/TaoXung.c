2e05a78048ae84dd4e28cd0cedf8ece93650b93c
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-05-26 00:29:54 +0700

    Update TaoXung.c

--- BaiTap/3-Timer/2/TaoXung.c
+++ BaiTap/3-Timer/2/TaoXung.c
@@ -1,3 +1,5 @@
+// Tạo xung trên chân RD1, tần số 50Hz nếu nút được nhấn, 100Hz nếu nút nhả
+
 #include <p18f4520.h>
 #include <timers.h>
 
@@ -15,14 +17,14 @@ void main()
 	OpenTimer1(TIMER_INT_OFF & T1_PS_1_1 & T1_SOURCE_INT);
 	while (1)
 	{
-		// neu nut nhan tren RE0 thi tat RE1
+		// neu nut nhan tren RE0 thi tat RD1
 		if (PORTCbits.RC0 == 0)
 		{
 			WriteTimer1(65536 - 10000);
 			// lap cho den khi timer1 tran
 			while (PIR1bits.TMR1IF == 0)
 				;
-			// dao nguoc RE1
+			// dao nguoc RD1
 			PORTDbits.RD1 = ~PORTDbits.RD1;
 			// xoa co tran
 			PIR1bits.TMR1IF = 0;
