ba97bdf8af4a3191f6f03f3b5eb45d35e0f876b3
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-05-25 23:58:06 +0700

    Create Timer 2

---
+++ BaiTap/3-Timer/2/TaoXung.c
@@ -0,0 +1,39 @@
+#include <p18f4520.h>
+#include <timers.h>
+
+#pragma config OSC = HS
+#pragma config WDT = OFF
+#pragma config MCLRE = ON
+
+void main()
+{
+	ADCON1 = 0x0F;
+	TRISCbits.RC0 = 1;
+	TRISDbits.RD1 = 0;
+
+	// Timer1 on, internal clock source, 1:1 prescaler, 16-bit mode
+	OpenTimer1(TIMER_INT_OFF & T1_PS_1_1 & T1_SOURCE_INT);
+	while (1)
+	{
+		// neu nut nhan tren RE0 thi tat RE1
+		if (PORTCbits.RC0 == 0)
+		{
+			WriteTimer1(65536 - 10000);
+			// lap cho den khi timer1 tran
+			while (PIR1bits.TMR1IF == 0)
+				;
+			// dao nguoc RE1
+			PORTDbits.RD1 = ~PORTDbits.RD1;
+			// xoa co tran
+			PIR1bits.TMR1IF = 0;
+		}
+		else
+		{
+			WriteTimer1(65536 - 5000);
+			while (PIR1bits.TMR1IF == 0)
+				;
+			PORTDbits.RD1 = ~PORTDbits.RD1;
+			PIR1bits.TMR1IF = 0;
+		}
+	}
+}
