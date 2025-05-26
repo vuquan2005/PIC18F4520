cb84253e86f40f56e054ba36c8c3667c9c4f287f
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-05-26 16:42:26 +0700

    update tpwm 2

--- BaiTap/6-PWM/2/Template.c
+++ BaiTap/6-PWM/2/Template.c
@@ -6,48 +6,36 @@
 #pragma config WDT = OFF
 #pragma config PBADEN = OFF
 
-char x = 0;
-
-void ngat_ngoai(void);
-#pragma code uu_tien_cao = 0x08
-void ngat_cao(void)
-{
-	ngat_ngoai();
-}
-#pragma code
-#pragma interrupt ngat_ngoai
-void ngat_ngoai(void)
-{
-	if (INTCON3bits.INT1IF == 1)
-	{
-		INTCON3bits.INT1IF = 0;
-		x++;
-		if (x > 3)
-			x = 1;
-	}
-}
-
 void main(void)
 {
 	ADCON1 = 0x0f;
 
-	INTCONbits.GIE = 1;
-	INTCONbits.INT0IE = 1;
-	INTCON2bits.INTEDG0 = 0;
-	INTCONbits.INT0IF = 0;
-
-	TRISC = 0;
+	TRISB = 0b00000001;
+	TRISC = 0b00000000;
 	T2CONbits.T2CKPS1 = 1;
 	T2CONbits.T2CKPS0 = 1;
 
 	OpenPWM1(149);
 	while (1)
 	{
-		if (x == 1)
-			SetDCPWM1(120);
-		else if (x == 2)
-			SetDCPWM1(240);
-		else if (x == 3)
-			SetDCPWM1(480);
+		while (PORTBbits.RB0 == 0)
+			;
+		while (PORTBbits.RB0 == 1)
+			;
+		SetDCPWM1(120);
+
+		while (PORTBbits.RB0 == 0)
+			;
+		while (PORTBbits.RB0 == 1)
+			;
+
+		SetDCPWM1(240);
+
+		while (PORTBbits.RB0 == 0)
+			;
+		while (PORTBbits.RB0 == 1)
+			;
+
+		SetDCPWM1(480);
 	}
 }
