7a0f2501128708a25b974d60c28ade3c5edf4225
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-05-27 00:54:21 +0700

    !!! update all path to mcc18

--- BaiTap/6-PWM/2/Template.c
+++ BaiTap/6-PWM/2/Template.c
@@ -1,3 +1,5 @@
+// Tạo xung PWM ở chân RC1 T=1200us, 8MHz, xung theo số lần nhấn nút RB0
+
 #include <P18f4520.h>
 #include <pwm.h>
 
@@ -12,30 +14,32 @@ void main(void)
 
 	TRISB = 0b00000001;
 	TRISC = 0b00000000;
+	// Hệ số chia 1:16
 	T2CONbits.T2CKPS1 = 1;
 	T2CONbits.T2CKPS0 = 1;
-
-	OpenPWM1(149);
+	// Tạo xung PWM ở chân RC1/CCP2B => sử dụng PWM2
+	OpenPWM2(149);
 	while (1)
 	{
 		while (PORTBbits.RB0 == 0)
 			;
 		while (PORTBbits.RB0 == 1)
 			;
-		SetDCPWM1(120);
+		// 20%
+		SetDCPWM2(120);
 
 		while (PORTBbits.RB0 == 0)
 			;
 		while (PORTBbits.RB0 == 1)
 			;
-
-		SetDCPWM1(240);
+		// 40%
+		SetDCPWM2(240);
 
 		while (PORTBbits.RB0 == 0)
 			;
 		while (PORTBbits.RB0 == 1)
 			;
-
-		SetDCPWM1(480);
+		// 80%
+		SetDCPWM2(480);
 	}
 }
