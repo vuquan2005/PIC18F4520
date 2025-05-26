240b6f5f2e642746c87384d520f4dbd787665df7
Vũ Quân <66623851+vuquan2005@users.noreply.github.com>
2025-05-27 01:13:48 +0700

    updtae template

---
+++ Template/Template.c
@@ -0,0 +1,27 @@
+#include <p18f4520.h>
+// #include <delays.h>
+// #include <timers.h>
+// #include <access.h>
+// #include <pwm.h>
+// #include <usart.h>
+// #include <stdio.h>
+
+#pragma config OSC = HS
+#pragma config MCLRE = ON
+#pragma config WDT = OFF
+#pragma config PBADEN = OFF
+//// Config cần cho USART
+// #pragma config PWRT = ON
+// #pragma config BOREN = OFF
+// #pragma config LVP = OFF
+
+void main(void)
+{
+    ADCON1 = 0x0f;
+    
+    while (1)
+    {
+
+    }
+}
+
